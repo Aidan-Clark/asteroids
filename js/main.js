0025787d97bb21155457eef381fe302db3fa156f
unknown <AClark@spartaglobal.com>
2018-12-15 21:36:20 +0000

    Now detects wasd on key hold, & movement tied to refresh rate

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(function() {
   var $ship = $(".ship");
-  var baseMovement = 10;
+  var baseMovement = 5;
+  var shipVelocity = [0, 0];
+  var kHeldDown = false;
+  var wasdKeys = [false, false, false, false];
 
   // Get left: & top: CSS values
   var shipX = parseFloat($ship.css("left").match(/\d/g).join(""));
@@ -13,36 +16,14 @@ $(function() {
   shipTheta = Math.round(Math.acos(shipTheta) * (180/Math.PI))
 
 
-  function moveForwards() {
-    console.log(shipTheta);
-    shipX += baseMovement * Math.sin(shipTheta * (Math.PI/180));
-    shipY -= baseMovement * Math.cos(shipTheta * (Math.PI/180));
-    $ship.css({"left": shipX + "px", "top": shipY + "px"})
+  function shipMovement(direction) {
+    shipX += direction * baseMovement * Math.sin(shipTheta * (Math.PI/180));
+    shipY -= direction * baseMovement * Math.cos(shipTheta * (Math.PI/180));
+    $ship.css({"left": shipX + "px", "top": shipY + "px"});
   }
 
-  function moveBackwards() {
-    console.log(shipTheta);
-    shipX -= baseMovement * Math.sin(shipTheta * (Math.PI/180));
-    shipY += baseMovement * Math.cos(shipTheta * (Math.PI/180));
-    $ship.css({"left": shipX + "px", "top": shipY + "px"})
-  }
-
-  function rotateLeft() {
-    shipTheta -= 10;
-    if (shipTheta < 0) {
-      shipTheta += 360;
-    }
-
-    if (shipTheta >= 360) {
-      shipTheta -= 360;
-    }
-
-    $ship.css("transform", "rotate(" + shipTheta + "deg)");
-    console.log(shipTheta);
-  }
-
-  function rotateRight() {
-    shipTheta += 10;
+  function shipRotation(direction) {
+    shipTheta += direction * 5;
     if (shipTheta < 0) {
       shipTheta += 360;
     }
@@ -51,31 +32,77 @@ $(function() {
       shipTheta -= 360;
     }
     $ship.css("transform", "rotate(" + shipTheta + "deg)");
-    console.log(shipTheta);
   }
+
+  setInterval(function() {
+    if (wasdKeys[0] == true) {
+      shipMovement(1);
+    }
+
+    if (wasdKeys[1] == true) {
+      shipRotation(-1);
+    }
+
+    if (wasdKeys[2] == true) {
+      shipMovement(-1);
+    }
+
+    if (wasdKeys[3] == true) {
+      shipRotation(1);
+    }
+
+  }, 17);
 
   $(document).keydown(function(event) {
     switch(event.which) {
-        case 87: // w
-          moveForwards();
-          break;
+      case 87: // w
+        wasdKeys[0] = true;
+        break;
 
-        case 65: // a
-          rotateLeft();
-          break;
+      case 65: // a
+        wasdKeys[1] = true;
+        break;
 
-        case 83: // s
-          moveBackwards();
-          break;
+      case 83: // s
+        wasdKeys[2] = true;
+        break;
 
-        case 68: // d
-          rotateRight();
-          break;
+      case 68: // d
+        wasdKeys[3] = true;
+        break;
 
-        default: return; // exit this handler for other keys
+      default: return;
     }
-    event.preventDefault(); // prevent the default action (scroll / move caret)
+    event.preventDefault();
   });
+
+
+  $(document).keyup(function(event) {
+    switch(event.which) {
+      case 87: // w
+        wasdKeys[0] = false;
+        break;
+
+      case 65: // a
+        wasdKeys[1] = false;
+        break;
+
+      case 83: // s
+        wasdKeys[2] = false;
+        break;
+
+      case 68: // d
+        wasdKeys[3] = false;
+        break;
+
+        default: return;
+    }
+    event.preventDefault();
+  });
+
+
+
+
 
 
 
